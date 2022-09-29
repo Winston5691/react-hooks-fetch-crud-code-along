c90c1f274592baf544a9245d631dce2c9d6bdc37
Winston Langat <winston.langat@student.moringaschool.com>
2022-09-29 19:28:21 +0300

    Done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,47 +1,45 @@
-import React from "react";
+import React from 'react'
 
 function Item({ item, onUpdateItem, onDeleteItem }) {
   function handleAddToCartClick() {
+    // console.log('clicked item:', item)
     fetch(`http://localhost:4000/items/${item.id}`, {
-
-     method: "PATCH",
-     headers: {
-      "Content-Type" : "application/json"
-     },
-     body: JSON.stringify({
-
-      isInCart: !item.isInCart
-     })
-    
-   })
-
-   .then(res => res.json())
-    .then((updatedItem) => onUpdateItem(updatedItem))
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        isInCart: !item.isInCart,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => onUpdateItem(updatedItem))
   }
 
-  function handleDeleteClick(){
+  function handleDeleteClick() {
+    console.log(item)
     fetch(`http://localhost:4000/items/${item.id}`, {
-      method: "DELETE",
-
+      method: 'DELETE',
     })
-    .then(res => res.json())
-    .then(() => onDeleteItem(item))
-   // console.log(item)
+      .then((r) => r.json())
+      .then(() => onDeleteItem(item));
   }
 
   return (
-    <li className={item.isInCart ? "in-cart" : ""}>
+    <li className={item.isInCart ? 'in-cart' : ''}>
       <span>{item.name}</span>
-      <span className="category">{item.category}</span>
+      <span className='category'>{item.category}</span>
       <button
+        className={item.isInCart ? 'remove' : 'add'}
         onClick={handleAddToCartClick}
-        className={item.isInCart ? "remove" : "add"}
       >
-        {item.isInCart ? "Remove From" : "Add to"} Cart
+        {item.isInCart ? 'Remove From' : 'Add to'} Cart
       </button>
-      <button onClick={handleDeleteClick} className="remove">Delete</button>
+      <button className='remove' onClick={handleDeleteClick}>
+        Delete
+      </button>
     </li>
-  );
+  )
 }
 
-export default Item;
+export default Item
